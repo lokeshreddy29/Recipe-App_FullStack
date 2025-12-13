27ceb098179468a4ad9248fd1b118a240eebade6
lokeshreddy29 <lokeshr3ddy@gmail.com>
2025-12-13 20:15:11 +0530

    created react app with vite

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
-console.log("hello")
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+app.listen(PORT, () => {
+  console.log(`app has begun on port ${PORT}`)
+})
