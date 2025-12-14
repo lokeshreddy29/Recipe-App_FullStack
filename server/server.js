2966094b801a611a8e2e8bb32c062bab2ae011d3
lokeshreddy29 <lokeshr3ddy@gmail.com>
2025-12-14 14:14:58 +0530

    navigation bar

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import './src/routes/userRoutes'
 
 const app = express()
 const PORT = 3000
@@ -6,6 +7,8 @@ const PORT = 3000
 app.get("/", (req, res) => {
   res.send("Hello world")
 })
+
+app.use('/api', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`app has begun on port ${PORT}`)
